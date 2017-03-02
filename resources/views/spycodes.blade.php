a1c2084196aa81fe2fa6526d0aec58872a41d197
Erick M <batman@Batmobile.local>
2017-03-01 17:42:11 -0800

    Hack to fix endless height iframe

--- resources/views/spycodes.blade.php
+++ resources/views/spycodes.blade.php
@@ -17,6 +17,17 @@
 
     <script>
         window.Laravel = { csrfToken: '{{ csrf_token() }}' };
+
+        // Hack for then it's displayed through an iFrame with big ass Height
+        window.onload = function () {
+            if (window.self !== window.top) {
+                var containers = document.getElementsByClassName('cards-container');
+
+                if(containers.length > 0) {
+                    containers[0].style.maxHeight = "787px";
+                }
+            }
+        }
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.5.0/socket.io.min.js"></script> 
     <script src="{{ asset('js/app.js') }}"></script>
